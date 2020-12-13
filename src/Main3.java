d002d1e028fdd1c42e3bb2450574c68e3220d109
Jason Robert <jason.robert@etu.unice.fr>
2020-12-13 14:27:36 +0100

    moyenne

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+        Moyenne moyenne = new Moyenne(100, 0.4, 0.4);
+        System.out.println(moyenne.getAverage());
+    }
+
+
+}
---
+++ src/Moyenne.java
@@ -0,0 +1,22 @@
+public class Moyenne {
+
+    private double average;
+
+    public Moyenne(int nbVertices, double p , double q) {
+        average = averageFor100Graphs(nbVertices, p, q);
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public double averageFor100Graphs(int nbVertices, double p, double q){
+        for(int i = 0 ; i<100 ; i++){
+            Graph graph = new Graph(nbVertices, p, q);
+            average += graph.sequenceRouge();
+        }
+
+        return average/100;
+
+    }
+}
