ddfafc82beb6893e7150c7319418d1919e11984c
Jason Robert <jason.robert@etu.unice.fr>
2020-12-20 14:32:37 +0100

    main algo 2

---
+++ src/MainAlgo1.java
@@ -0,0 +1,57 @@
+
+public class MainAlgo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("-------------------   p = 0.1   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.1, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.2   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.2, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.3   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.3, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.4   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.4, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.5   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.5, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.6   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.6, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.7   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.7, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.8   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.8, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 0.9   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.9, (float)i/10, false).getAverage());
+        }
+
+        System.out.println("-------------------   p = 1   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 1, (float)i/10, false).getAverage());
+        }
+
+    }
+}
--- src/Moyenne.java
+++ src/Moyenne.java
@@ -2,21 +2,31 @@ public class Moyenne {
 
     private double average;
 
-    public Moyenne(int nbVertices, double p , double q) {
-        average = averageFor100Graphs(nbVertices, p, q);
+    public Moyenne(int nbVertices, double p, double q, boolean version2) {
+        if (version2)
+            average = averageFor100GraphsV2(nbVertices, p, q);
+        else
+            average = averageFor100Graphs(nbVertices, p, q);
     }
 
     public double getAverage() {
         return average;
     }
 
-    public double averageFor100Graphs(int nbVertices, double p, double q){
-        for(int i = 0 ; i<100 ; i++){
+    public double averageFor100Graphs(int nbVertices, double p, double q) {
+        for (int i = 0; i < 100; i++) {
+            Graph graph = new Graph(nbVertices, p, q);
+            average += graph.sequenceRouge();
+        }
+        return average / 100;
+    }
+
+    public double averageFor100GraphsV2(int nbVertices, double p, double q) {
+        for (int i = 0; i < 100; i++) {
             Graph graph = new Graph(nbVertices, p, q);
             average += graph.sequenceRouge2();
         }
-
-        return average/100;
-
+        return average / 100;
     }
+
 }
