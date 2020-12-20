8f51c2b911836b84367d7aaf0b5bd452dca1e542
Jason Robert <jason.robert@etu.unice.fr>
2020-12-20 14:43:37 +0100

    modif main

--- src/MainAlgo2.java
+++ src/MainAlgo2.java
@@ -2,54 +2,59 @@ public class MainAlgo2 {
 
     public static void main(String[] args) {
 
+        System.out.println("-------------------   p = 0   -------------------");
+        for(int i=0; i<10; i++){
+            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0, (float)i/10, true).getAverage());
+        }
+
         System.out.println("-------------------   p = 0.1   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.1, (float)i/10, true).getAverage());
+            System.out.println("p = 0.1 q = 0." + i + " --> " + new Moyenne(100, 0.1, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.2   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.2, (float)i/10, true).getAverage());
+            System.out.println("p = 0.2 q = 0." + i + " --> " + new Moyenne(100, 0.2, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.3   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.3, (float)i/10, true).getAverage());
+            System.out.println("p = 0.3 q = 0." + i + " --> " + new Moyenne(100, 0.3, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.4   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.4, (float)i/10, true).getAverage());
+            System.out.println("p = 0.4 q = 0." + i + " --> " + new Moyenne(100, 0.4, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.5   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.5, (float)i/10, true).getAverage());
+            System.out.println("p = 0.5 q = 0." + i + " --> " + new Moyenne(100, 0.5, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.6   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.6, (float)i/10, true).getAverage());
+            System.out.println("p = 0.6 q = 0." + i + " --> " + new Moyenne(100, 0.6, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.7   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.7, (float)i/10, true).getAverage());
+            System.out.println("p = 0.7 q = 0." + i + " --> " + new Moyenne(100, 0.7, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.8   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.8, (float)i/10, true).getAverage());
+            System.out.println("p = 0.8 q = 0." + i + " --> " + new Moyenne(100, 0.8, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 0.9   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 0.9, (float)i/10, true).getAverage());
+            System.out.println("p = 0.9 q = 0." + i + " --> " + new Moyenne(100, 0.9, (float)i/10, true).getAverage());
         }
 
         System.out.println("-------------------   p = 1   -------------------");
         for(int i=0; i<10; i++){
-            System.out.println("p = 0 q = 0." + i + " --> " + new Moyenne(100, 1, (float)i/10, true).getAverage());
+            System.out.println("p = 1 q = 0." + i + " --> " + new Moyenne(100, 1, (float)i/10, true).getAverage());
         }
 
     }
